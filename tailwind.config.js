ab324fd609f6ea9e4c57a75fefa68be05dbc349c
Feek18 <bintangfikri31@gmail.com>
2024-07-14 01:52:40 +0800

    done porto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,45 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['index.html'],
+//   darkMode : 'class',
+//   theme: {
+//     container: {
+//       center : true,
+//       padding : '16px'
+//     },
+//     extend: {
+//       colors:{
+//         primary : '#1d4ed8',
+//         secondary : '#64748b',
+//         dark : '#0f172a',
+//       },
+//       screens : {
+//         '2xl' : '1320px'
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  darkMode : 'class',
+  content: ["./*.{html,js}"],
+  darkMode: "class",
   theme: {
     container: {
-      center : true,
-      padding : '16px'
+      center: true,
+      padding: "16px",
     },
     extend: {
-      colors:{
-        primary : '#1d4ed8',
-        secondary : '#64748b',
-        dark : '#0f172a',
+      colors: {
+        primary: "#1d4ed8",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
-      screens : {
-        '2xl' : '1320px'
-      }
+      screens: {
+        "2xl": "1320px",
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
